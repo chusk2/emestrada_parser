b767d5f81054190e8d58dcf1ec82c490fadf2ee9
Daniel <294.daniel@gmail.com>
2025-02-23 23:37:30 +0100

    add telegram bot

---
+++ bot_credentials.py
@@ -0,0 +1,8 @@
+BOT_TOKEN = "7874470313:AAEca3QJal97r0AdMf-qfBG5XDcR3s8EOWs"
+CHAT_ID = "6188540451"
+
+# Token: 7874470313:AAEca3QJal97r0AdMf-qfBG5XDcR3s8EOWs
+# chat id: 6188540451
+# URL to get chat id:
+# https://api.telegram.org/bot7874470313:AAEca3QJal97r0AdMf-qfBG5XDcR3s8EOWs/getUpdates
+
