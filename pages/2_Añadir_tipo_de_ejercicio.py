5800e621c38b3b768b7d937a3659b1e0e20a4059
Daniel <294.daniel@gmail.com>
2025-12-05 14:21:54 +0100

    Finish add exercise type feature

--- pages/2_Añadir_tipo_de_ejercicio.py
+++ pages/2_Añadir_tipo_de_ejercicio.py
@@ -22,6 +22,9 @@ for key in ['subjects_key', 'topics_key', 'exercise_types_key']:
 if not "type_key" in state.keys():
     state["type"] = ""
 
+if not "topics" in state.keys():
+    state["topics"] = []
+
 def store_data():
     # retrieve data from current run
     current_subject = state['subject_key']
@@ -45,18 +48,22 @@ def reload_topics():
 
 st.title("Añadir un nuevo tipo de ejercicio")
 
-with st.form("form"):
 
-    st.selectbox('Asignatura', options = SUBJECTS, key = "subject_key",
-                 on_change=reload_topics)
-    
-    st.selectbox('Tema', options = state["topics"], key = "topics_key" )
 
-    st.text_input('Tipo de ejercicio', max_chars=200, key = "type_key")
+st.selectbox('Asignatura', options = SUBJECTS, key = "subject_key",
+                on_change=reload_topics)
 
-    st.form_submit_button('Guardar datos', on_click=store_data)
+st.selectbox('Tema', options = state["topics"], key = "topics_key" )
 
-st.button("Exportar datos", on_click=export_data)
+st.text_input('Tipo de ejercicio', max_chars=200, key = "type_key")
+
+col1, col2 = st.columns([1,1])
+
+with col1:
+    st.button('Guardar datos', on_click=store_data)
+
+with col2:
+    st.button("Exportar datos", on_click=export_data)
     
 
 
