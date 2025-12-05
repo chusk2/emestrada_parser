250cebeaf4b674361a9fdfc7d4babb8ffd8977ab
Daniel <294.daniel@gmail.com>
2025-12-05 12:28:57 +0100

    Finish classify exercise feature

--- classify_exercises.py
+++ classify_exercises.py
@@ -1,8 +1,7 @@
-import streamlit as st
-from PyPDF2 import PdfReader
-from pathlib import Path
 import json
+import streamlit as st
 import pandas as pd
+from PyPDF2 import PdfReader
 
 # list of subjects
 SUBJECTS = ['FÍSICA', 'QUÍMICA', 'MATEMÁTICAS II',
@@ -11,20 +10,45 @@ SUBJECTS = ['FÍSICA', 'QUÍMICA', 'MATEMÁTICAS II',
 # list to store the exercises in the file
 exercises = []
 
-# dictionary to store the classification data for each exercise
+## INITIALISE THE session_state keys
+
 state = st.session_state
-state['data'] = {key : [] for key in ['subject', 'year', 'topic', 'exam',
-                               'exercise', 'exercise_type', 'statement']}
+
+# dictionary to store the classification data for each exercise
+if 'data' not in state:
+    state['data'] = {key : [] for key in ['subject', 'year', 'topic', 'exam',
+                                   'exercise', 'exercise_type', 'statement']}
+    
+if not "exercise_type_key" in state.keys():
+    state["exercise_type_key"] = []
+if not "statement_key" in state.keys(): 
+    state['statement_key'] = ""
+if not "pdf_file" in state.keys():
+    state['pdf_file'] = None
+
+# after submit, store the data and reset the corresponding form widgets
+def store_data():
+    # store information into data dictionary
+    state['data']['year']           .append(year)
+    state['data']['subject']        .append(subject)
+    state['data']['topic']          .append(topic)
+    state['data']['exercise']       .append(state['exercise_key'])
+    state['data']['exam']           .append(state['exercise_key'].split('.')[0])
+    state['data']['exercise_type']  .append(state['exercise_type_key'])
+    state['data']['statement']      .append(state['statement_key'])
+    
+    # reset the widgets from the form
+    state["exercise_type_key"] = []
+    state['statement_key'] = ""
 
 # load the exercise types dictionary
 with open('exercise_types.json', 'r') as file:
     exercise_types_dict = json.load(file)
 
+## START UI ##
 st.title('Classify exercise')
 
-state = st.session_state
-
-pdf_file = st.file_uploader('Add pdf file')
+pdf_file = st.file_uploader('Add pdf file', key = "pdf_form")
 
 if pdf_file:
 
@@ -60,37 +84,31 @@ if pdf_file:
     st.write(f'### Classify exercises for *{topic}* from year **{year}**')
 
     with st.form(key = 'form'):
-        form_data = {
-            'exercise' : st.selectbox('Exercise', options = exercises),
-            'exercise_type' : st.multiselect('Tipo de ejercicio', options = exercise_types_list),
-            'statement' : st.text_area('Insert exercise statement here')
-        }
+        st.selectbox('Exercise', options = exercises, key = "exercise_key")
+        st.multiselect('Tipo de ejercicio', options = exercise_types_list,
+                                           key = "exercise_type_key")
+        st.text_area('Insert exercise statement here', key = "statement_key")
 
-        st.form_submit_button('Guardar datos')
-    
-    # add the data for current exercise to classification data dictionary
-    classification_data = {
-        'year' : year,
-        'subject' : subject,
-        'topic' : topic,
-        'exercise' : form_data['exercise'],
-        'exercise_type' : form_data['exercise_type'],
-        'statement' : form_data['statement']
-    }
+        submitted  = st.form_submit_button('Guardar datos', on_click=store_data)
 
-    for key, value in classification_data.items():
-        state['data'][key].append(value)
-    
-    st.write(state['data'])
-    # columns for csv file: subject,year,topic,exam,type
-    st.write('Classification data for current exercise:')
-    st.write(form_data)
-    st.write(len(state['data']))
-    if st.button('Export data'):
-        # csv columns:
-        # subject,year,topic,exam,exercise,exercise_type,statement
-        df = pd.read_csv('data.csv')
-        classified_df = pd.DataFrame(state['data'], index = range(len(state['data'])))
-        df = pd.concat([df, classified_df]).drop_duplicates()
+    if st.button('Save data into csv', key = "save_csv"):
+        
+        # start export process only when at least one exercise has been classified
+        if len(state['data']['subject']) > 0:
 
-    
+            st.divider()
+            st.write('Classification data for current exercise:')
+
+            # create dataframe from classification data
+            classified_df = pd.DataFrame(state['data'], index = range(len(state['data']['subject'])))
+            # explode exercise_type column
+            classified_df = classified_df.explode('exercise_type')
+            # show the classified data as dataframe
+            st.write(classified_df)
+
+            # merge the classification dataset to existing data
+            df = pd.read_csv('data.csv')
+            df = pd.concat([df, classified_df]).drop_duplicates()
+            
+        else:
+            st.warning("There are no values stored to export to a csv file!")
